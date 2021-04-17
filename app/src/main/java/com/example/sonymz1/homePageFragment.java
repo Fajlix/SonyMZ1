de3d16e45e0dfed3dade3d9f18c75ad92704b019
viktorwcfc <viktorwcfc@gmail.com>
2021-04-17 13:22:15 +0200

    created challenge button

--- app/src/main/java/com/example/sonymz1/homePageFragment.java
+++ app/src/main/java/com/example/sonymz1/homePageFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+public class homePageFragment extends Fragment {
+
+    private ImageView addChallengeButton;
+
 
     @Override
     public View onCreateView(
@@ -17,18 +23,23 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.addChallengeButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(homePageFragment.this)
+                        .navigate(R.id.action_homePageFragment_to_FirstFragment);
             }
         });
     }
+
+    private void initiateView(View view) {
+        addChallengeButton = view.findViewById(R.id.addChallengeButton);
+    }
+
 }
