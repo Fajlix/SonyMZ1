cde7514b5a0f17fbb47e316ec2dcbdd098779d6d
TJOCKochFIN <viktorhjoh@gmail.com>
2021-04-21 14:14:37 +0200

    added some comments/javadoc

--- app/src/main/java/com/example/sonymz1/CreateChallengeViewModel.java
+++ app/src/main/java/com/example/sonymz1/CreateChallengeViewModel.java
@@ -1,11 +1,21 @@
 package com.example.sonymz1;
 
+/**
+ * @author Viktor
+ */
 public class CreateChallengeViewModel {
     private Challenge challenge;
 
     public CreateChallengeViewModel() {
     }
 
+    /**
+     * Method for communication between Model and View, in this case Challenge and CreateChallengeFragment
+     * @param challengeName The name of the challenge
+     * @param challengeDescription A short description of hte challenge
+     * @param isPrivate A boolean to say if the challenge is private or not
+     * @param playerIDs A unique int for each user
+     */
     public void createChallenge(String challengeName, String challengeDescription, boolean isPrivate, int[] playerIDs)
     {
         challenge = new Challenge(challengeName);
@@ -14,6 +24,10 @@ public class CreateChallengeViewModel {
         addPlayers(playerIDs);
     }
 
+    /**
+     * Because we can add multiple users in the fragment but only one user at a time in the model
+     * @param playerIDs
+     */
     private void addPlayers (int[] playerIDs)
     {
         for (int i = 0; i < playerIDs.length; i++) {
