3291ea15d88644b86d8fa577a87b8b9b6d09f980
Wendy <pau.vendy@gmail.com>
2021-04-21 16:31:14 +0200

    Created pedestal and leaderboard

--- app/src/main/java/com/example/sonymz1/ChallengePageFragment.java
+++ app/src/main/java/com/example/sonymz1/ChallengePageFragment.java
@@ -2,11 +2,23 @@ package com.example.sonymz1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +26,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ChallengePageFragment extends Fragment {
+
+    private final Challenge challenge = new Challenge("Challenge");
+    private AllUsers users = new AllUsers();
+    private LeaderboardAdapter leaderboardAdapter;
+    private ImageView userImg1, userImg2, userImg3;
+    private TextView progressTxt1, progressTxt2, progressTxt3, moreBtn;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView rvcLeaderboard;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +80,63 @@ public class ChallengePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_challenge_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeViews(view);
+
+        challenge.addPlayer(users.getUserMap().get(1).getId(),6);
+        challenge.addPlayer(users.getUserMap().get(2).getId(),5);
+        challenge.addPlayer(users.getUserMap().get(3).getId(),10);
+        setPedestal();
+
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        rvcLeaderboard.setLayoutManager(linearLayoutManager);
+        leaderboardAdapter = new LeaderboardAdapter(getActivity(),challenge.getLeaderBoard());
+        rvcLeaderboard.setAdapter(leaderboardAdapter);
+
+        if(challenge.getLeaderBoard().size() > 3){
+            moreBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Test
+                    Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT).show();
+
+                    // show participants view
+                }
+            });
+        }else moreBtn.setVisibility(View.GONE);
+    }
+
+    /**
+     * Set the top 3 users info on the pedestal.
+     */
+    private void setPedestal(){
+        // Case if there are only 2 users??
+        List<Map.Entry<Integer, Integer>> leaderboardList =
+                new LinkedList<>(challenge.getLeaderBoard().entrySet());
+        userImg1.setImageResource(users.getUserMap().get(leaderboardList.get(0).getKey()).getProfilePic());
+        userImg2.setImageResource(users.getUserMap().get(leaderboardList.get(1).getKey()).getProfilePic());
+        userImg3.setImageResource(users.getUserMap().get(leaderboardList.get(2).getKey()).getProfilePic());
+        progressTxt1.setText(String.valueOf(leaderboardList.get(0).getValue()));
+        progressTxt2.setText(String.valueOf(leaderboardList.get(1).getValue()));
+        progressTxt3.setText(String.valueOf(leaderboardList.get(2).getValue()));
+    }
+
+    /**
+     * Instantiate all the required views.
+     * @param view the fragments view.
+     */
+    private void initializeViews(View view){
+        userImg1 = view.findViewById(R.id.user_img1);
+        userImg2 = view.findViewById(R.id.user_img2);
+        userImg3 = view.findViewById(R.id.user_img3);
+        progressTxt1 = view.findViewById(R.id.progressTxt1);
+        progressTxt2 = view.findViewById(R.id.progressTxt2);
+        progressTxt3 = view.findViewById(R.id.progressTxt3);
+        rvcLeaderboard = view.findViewById(R.id.rvcLeaderboard);
+        moreBtn = view.findViewById(R.id.moreBtn);
     }
 }
---
+++ app/src/main/java/com/example/sonymz1/LeaderboardAdapter.java
@@ -0,0 +1,76 @@
+package com.example.sonymz1;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Adapter for populating the recyclerview for the leaderboard.
+ *
+ * TODO Fix so that the logged in user is shown in the leaderboard.
+ * @author Wendy
+ */
+public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.ViewHolder> {
+    private Map<Integer,Integer> leaderboard;
+    private Activity context;
+    private AllUsers users;
+
+    public LeaderboardAdapter(Activity context, Map<Integer, Integer> leaderboard) {
+        this.leaderboard = leaderboard;
+        this.context = context;
+        this.users = new AllUsers();
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.leaderboard_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        List<Map.Entry<Integer, Integer>> leaderboardList =
+                new LinkedList<>(leaderboard.entrySet());
+        User user = users.getUserMap().get(leaderboardList.get(position).getKey());
+        holder.usernametxt.setText(user.getUsername());
+        holder.progressTxt.setText(String.valueOf(leaderboardList.get(position).getValue())); // add unit
+        holder.userImg.setImageResource(user.getProfilePic());
+        switch (position) {
+            case 0: holder.rank.setText(1 + "st"); break;
+            case 1: holder.rank.setText(2 + "nd"); break;
+            case 2: holder.rank.setText(3 + "rd"); break;
+            default: holder.rank.setText(position+1 + "th"); break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView usernametxt, progressTxt, rank;
+        ImageView userImg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernametxt = itemView.findViewById(R.id.username);
+            progressTxt = itemView.findViewById(R.id.userProgressTxt);
+            rank = itemView.findViewById(R.id.rankTxt);
+            userImg = itemView.findViewById(R.id.userImg);
+        }
+    }
+}
