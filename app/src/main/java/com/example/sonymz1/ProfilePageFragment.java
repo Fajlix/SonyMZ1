fcc7fd6e165d37f8a9228bc123e14809b56ef2ba
Wendy <pau.vendy@gmail.com>
2021-05-20 13:23:52 +0200

    Removed some hardcoded code

--- app/src/main/java/com/example/sonymz1/ProfilePageFragment.java
+++ app/src/main/java/com/example/sonymz1/ProfilePageFragment.java
@@ -22,54 +22,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfilePageFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * @author Elina W, Viktor W
  */
 public class ProfilePageFragment extends Fragment {
 
     private ChallengeViewModel vm;
     private RecyclerView rvcfriendList;
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ProfilePageFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfilePageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfilePageFragment newInstance(String param1, String param2) {
-        ProfilePageFragment fragment = new ProfilePageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -85,10 +51,12 @@ public class ProfilePageFragment extends Fragment {
         vm = new ViewModelProvider(requireActivity()).get(ChallengeViewModel.class);
         initializeViews(view);
         rvcfriendList.setLayoutManager(new LinearLayoutManager(getContext()));
-        //TODO: Fix friendlist in DB.
-        vm.getMainUser().setFriends(new ArrayList<>());
-        vm.getMainUser().getFriends().add(new User("test1", 0));
-        vm.getMainUser().getFriends().add(new User("test2", 1));
+
+        /* Test
+        vm.getMainUser().getFriends().add(new User("Friend 1", 0));
+        vm.getMainUser().getFriends().add(new User("Friend 2", 1));
+         */
+
         ProfileAdapter profileAdapter = new ProfileAdapter(this,
                 vm.getMainUser().getFriends());
         rvcfriendList.setAdapter(profileAdapter);
